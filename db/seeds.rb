c5e5a6eee5a800f5616cfeb77939a66351d57e83
Leandro Iamele <leandroiamele@gmail.com>
2019-07-12 10:57:30 -0300

    Cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts "Cleaning Database"
+Ingredient.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+open = open(url).read
+json = JSON.parse(open)
+
+puts ""
+puts "Creating seeds"
+json["drinks"].each do |ingredient|
+  i = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "- created #{i.name}"
+end
+
+puts "Masterclass seeds"
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Cuba Libre")
+Cocktail.create(name: "Ruso Negro")
+
+
+puts ""
+puts "Finished seeding process!"
